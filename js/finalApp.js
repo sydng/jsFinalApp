444bd167f2f0444e417d3db81a683f56c9ce84d2
sydng <35381098+sydng@users.noreply.github.com>
2018-05-05 13:18:48 -0400

    adding in phases for checkboxes

--- js/finalApp.js
+++ js/finalApp.js
@@ -9,65 +9,81 @@ var map = new mapboxgl.Map({
 
 map.addControl(new mapboxgl.NavigationControl());
 
+//GEOJSON URLs
+
+var phase1 = "https://raw.githubusercontent.com/sydng/Datasets/master/phase1_geojson.geojson";
+var phase2 = "https://raw.githubusercontent.com/sydng/Datasets/master/phase2_geojson.geojson";
+var phase3 = "https://raw.githubusercontent.com/sydng/Datasets/master/phase3_geojson.geojson";
+
 //CREATE WEBPAGE
 $(document).ready(function(){
-  $('#sidebar').hide();
-  $('#about').hide();
-  $('#myModal').modal('show');
+  $.ajax(promise).done(function(data) {
+    var parsedData = JSON.parse(data);
+    phases = L.geoJson(parsedData, {
+      color: "lightgray",
+      filter: majorArterials
+    }).bindPopup(function(layer) {
+      return layer.feature.properties.hundred_block_score.toString();
+    });
 
-  $('#exploreButton').on('click', function() {
-    $('#sidebar').show();
-  });
-
-  $('#inundBox').click(function() {
-    if($('#inundBox').prop("checked")) {
-      map.addLayer({
-        "id": "woodlands",
-        "type": "heatmap",
-        "source": {
-          "type": "vector",
-          "tiles": ["https://s3.us-east-2.amazonaws.com/jswoods/{z}/{x}/{y}.pbf"],
-          "minzoom": 0,
-          "maxzoom": 19
-        },
-        "source-layer": "WoodlandsPoints",
-        "paint": {
-          "heatmap-weight": [
-              "interpolate",
-              ["linear"],
-              ["get", "Inundation"],
-              0, 0,
-              3, 1
-          ],
-          "heatmap-color": [
-              "interpolate",
-              ["linear"],
-              ["heatmap-density"],
-              0, "rgba(33,102,172,0)",
-              0.2, "rgb(103,169,207)",
-              0.4, "rgb(209,229,240)",
-              0.6, "rgb(253,219,199)",
-              0.8, "rgb(239,138,98)",
-              1, "rgb(178,24,43)"
-          ],
-          'heatmap-opacity': 0.8
-        }
-      });
-    } else {
-      map.removeLayer("woodlands");
-    }
-  });
-
-  $('#aboutButton').on('click', function() {
-    $('#myModal').modal('show');
-    $('#map').css('top', '75px');
-  });
-
-  $('#resourceButton').on('click', function() {
-    $('#map').css('top', 'auto');
-  });
-
-  $('#closeIcon').on('click', function() {
     $('#sidebar').hide();
+    $('#about').hide();
+    $('#myModal').modal('show');
+
+    $('#exploreButton').on('click', function() {
+      $('#sidebar').show();
+    });
+
+    $('#inundBox').click(function() {
+      if($('#inundBox').prop("checked")) {
+        map.addLayer({
+          "id": "woodlands",
+          "type": "heatmap",
+          "source": {
+            "type": "vector",
+            "tiles": ["https://s3.us-east-2.amazonaws.com/jswoods/{z}/{x}/{y}.pbf"],
+            "minzoom": 0,
+            "maxzoom": 19
+          },
+          "source-layer": "WoodlandsPoints",
+          "paint": {
+            "heatmap-weight": [
+                "interpolate",
+                ["linear"],
+                ["get", "Inundation"],
+                0, 0,
+                3, 1
+            ],
+            "heatmap-color": [
+                "interpolate",
+                ["linear"],
+                ["heatmap-density"],
+                0, "rgba(33,102,172,0)",
+                0.2, "rgb(103,169,207)",
+                0.4, "rgb(209,229,240)",
+                0.6, "rgb(253,219,199)",
+                0.8, "rgb(239,138,98)",
+                1, "rgb(178,24,43)"
+            ],
+            'heatmap-opacity': 0.8
+          }
+        });
+      } else {
+        map.removeLayer("woodlands");
+      }
+    });
+
+    $('#aboutButton').on('click', function() {
+      $('#myModal').modal('show');
+      $('#map').css('top', '75px');
+    });
+
+    $('#resourceButton').on('click', function() {
+      $('#map').css('top', 'auto');
+    });
+
+    $('#closeIcon').on('click', function() {
+      $('#sidebar').hide();
+    });
   });
 });
