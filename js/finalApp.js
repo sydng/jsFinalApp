95ebbd83bd9932e575b6bc3524bbaa78da08d628
sydng <35381098+sydng@users.noreply.github.com>
2018-05-05 13:02:04 -0400

    changing map display

--- js/finalApp.js
+++ js/finalApp.js
@@ -58,45 +58,6 @@ $(document).ready(function(){
     }
   });
 
-  $('#devBox').click(function() {
-    if($('#devBox').prop("checked")) {
-      map.addLayer({
-        "id": "woodlands",
-        "type": "heatmap",
-        "source": {
-          "type": "vector",
-          "tiles": ["https://s3.us-east-2.amazonaws.com/jswoods/{z}/{x}/{y}.pbf"],
-          "minzoom": 0,
-          "maxzoom": 19
-        },
-        "source-layer": "WoodlandsPoints",
-        "paint": {
-          "heatmap-weight": [
-              "interpolate",
-              ["linear"],
-              ["get", "Developed"],
-              0, 0,
-              10, 1
-          ],
-          "heatmap-color": [
-              "interpolate",
-              ["linear"],
-              ["heatmap-density"],
-              0, "rgba(33,102,172,0)",
-              0.2, "rgb(103,169,207)",
-              0.4, "rgb(209,229,240)",
-              0.6, "rgb(253,219,199)",
-              0.8, "rgb(239,138,98)",
-              1, "rgb(178,24,43)"
-          ],
-          'heatmap-opacity': 0.8
-        }
-      });
-    } else {
-      map.removeLayer("woodlands");
-    }
-  });
-
   $('#aboutButton').on('click', function() {
     $('#myModal').modal('show');
     $('#map').css('top', '75px');
