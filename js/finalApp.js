d8cd5d29d216d0c4a4457d8164d3ac64e0f033a3
sydng <35381098+sydng@users.noreply.github.com>
2018-05-05 12:52:25 -0400

    three checkboxes

--- js/finalApp.js
+++ js/finalApp.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
   });
 
   $('#inundBox').click(function() {
-    if($('#inundBox').prop("disabled", false)) {
+    if($('#inundBox').prop("checked")) {
       map.addLayer({
         "id": "woodlands",
         "type": "heatmap",
@@ -53,7 +53,48 @@ $(document).ready(function(){
           'heatmap-opacity': 0.8
         }
       });
-    } else { }
+    } else {
+      map.removeLayer("woodlands");
+    }
+  });
+
+  $('#devBox').click(function() {
+    if($('#devBox').prop("checked")) {
+      map.addLayer({
+        "id": "woodlands",
+        "type": "heatmap",
+        "source": {
+          "type": "vector",
+          "tiles": ["https://s3.us-east-2.amazonaws.com/jswoods/{z}/{x}/{y}.pbf"],
+          "minzoom": 0,
+          "maxzoom": 19
+        },
+        "source-layer": "WoodlandsPoints",
+        "paint": {
+          "heatmap-weight": [
+              "interpolate",
+              ["linear"],
+              ["get", "Developed"],
+              0, 0,
+              10, 1
+          ],
+          "heatmap-color": [
+              "interpolate",
+              ["linear"],
+              ["heatmap-density"],
+              0, "rgba(33,102,172,0)",
+              0.2, "rgb(103,169,207)",
+              0.4, "rgb(209,229,240)",
+              0.6, "rgb(253,219,199)",
+              0.8, "rgb(239,138,98)",
+              1, "rgb(178,24,43)"
+          ],
+          'heatmap-opacity': 0.8
+        }
+      });
+    } else {
+      map.removeLayer("woodlands");
+    }
   });
 
   $('#aboutButton').on('click', function() {
